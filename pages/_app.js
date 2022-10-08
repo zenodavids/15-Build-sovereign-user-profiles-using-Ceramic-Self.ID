4e86f3957992757b37cb45c3a0b604bb1ac3cb02
zenodavids <chidozieezeanekwe@gmail.com>
2022-10-08 15:37:55 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+// import the Provider from Self.ID
+import { Provider } from "@self.id/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    //  wrap every component of ours with the Self.ID provider.
+    // we want the Provider to connect to the Clay Test Network for Ceramic.
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+};
 
-export default MyApp
+export default MyApp;
